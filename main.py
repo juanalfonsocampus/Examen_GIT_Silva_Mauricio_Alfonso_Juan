f2038ab3bae327413b8a1d965e8b7207bf69b790
juanalfonsocampus <juanalfonsocampus@gmail.com>
2024-07-31 13:30:23 -0500

    Good to go

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import functions
 print("*****************************")
 print("Welcome to our city register!")
 print("*****************************")
-def main():
+
+
     
-    while True:
-        print("1. Add a new city")
-        print("2. Modify city information")
-        print("3. Delete a city")
-        print("4. Find a city using specific criteria")
-        opc=int(input(""))
-        if opc==1:
-            functions.registrar_ciudades()
-        
-        elif opc==2:
-            print("Coming soon!")
-            
-        
-        elif opc==3:
-            functions.eliminar_ciudad()
+print ("Please select an option: ")
+print("1. Add a new city")
+print("2. Modify city information")
+print("3. Delete a city")
+print("4. Find a city using specific criteria")
+ 
+opc=int(input(""))
+ 
+if opc==1:
+    functions.registrar_ciudades()        
+elif opc==2:
+    print("Coming soon!")     
+elif opc==3:
+    functions.eliminar_ciudad()
