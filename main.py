49c4d64565776a216ae5342b1cb6889d4d42d6aa
mauriciosilva26 <eduardmauriciosilva876@gmail.com>
2024-07-31 12:46:16 -0500

    navigation menu

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print ("Hola mundo")
+impor 
+print("*****************************")
+print("Welcome")
+print("*****************************")
+def main():
+    opc=int(input(""))
+    if opc==1:
+        
+    elif opc==2:
+        
+    elif opc==3:
